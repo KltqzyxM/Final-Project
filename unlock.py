5cadd47ca8c75bfd924b80a2f62d970be67f5328
KltqzyxM <100015301+KltqzyxM@users.noreply.github.com>
2022-04-08 21:50:18 +0700

    Create unlock.py

---
+++ unlock.py
@@ -0,0 +1,11 @@
+from web3 import Web3
+import json
+from time import sleep
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+GPIO.output(18, 1)
+#Wait 1 Seconds
+sleep(1)
